6e8b3702ee3ceb0a866d4413e363f87940a208fd
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2016-11-17 19:53:05 +0300

    Initial commit

---
+++ 5010D-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCL/5010D/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCL/5010D/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCL/5010D/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCL/5010D/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCL/5010D/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCL/5010D/vendor,system/vendor)
